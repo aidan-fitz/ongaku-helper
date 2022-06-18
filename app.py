aeddcde73e44656675da676eefcf749c83446ad8
Evelyn Fitzgerald <acf67@cornell.edu>
2022-06-18 16:56:38 -0400

    Change content-type depending on client Apply black formatting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 
 from furigana import to_html, to_plaintext
 
@@ -8,15 +8,15 @@ app = Flask(__name__)
 @app.route("/furigana", methods=["POST"])
 def get_furigana():
     # For simplicity, get the entire request body as a string
-    request_body = request.data.decode(
-        request.mimetype_params.get("charset", "utf-8")
-    )
+    request_body = request.data.decode(request.mimetype_params.get("charset", "utf-8"))
     # Generate furigana in the format requested by the client
     # (HTML if possible, plaintext otherwise)
     if request.accept_mimetypes.accept_html:
         return to_html(request_body)
     else:
-        return to_plaintext(request_body)
+        return make_response(
+            (to_plaintext(request_body), {"Content-Type": "text/plain; charset=utf-8"})
+        )
 
 
 @app.route("/")
